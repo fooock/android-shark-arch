26ac23a64b016830f98165405497849ef627acae
newhouse <nhitbh@gmail.com>
2018-03-25 21:27:04 +0200

    Base class for compat activities

---
+++ arch-presenter/src/main/java/com/fooock/sharkarch/presenter/BaseCompatActivity.java
@@ -0,0 +1,11 @@
+package com.fooock.sharkarch.presenter;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Base class for all {@link android.app.Activity}s that need to extend
+ * {@link AppCompatActivity}
+ */
+public class BaseCompatActivity extends AppCompatActivity {
+
+}
